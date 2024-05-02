207498b43a92f049b4702366c47868062e5152b9
DanielPscolovki <151840169+DanielPscolovki@users.noreply.github.com>
2024-05-02 17:55:35 -0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+// 1. Convert funnyNumber to String
+let funnyNumber = 12345;
+let convertedFunnyNumberToString = String(funnyNumber);
+console.log(typeof convertedFunnyNumberToString, convertedFunnyNumberToString);
+
+// 2. Convert funnyString to Number
+let funnyString = '12345';
+let convertedFunnyStringToNumber = Number(funnyString);
+console.log(typeof convertedFunnyStringToNumber, convertedFunnyStringToNumber);
+
+// 3. Convert funnyTrue to Boolean
+let funnyTrue = '1';
+let convertedFunnyTrueToBoolean = Boolean(funnyTrue);
+console.log(typeof convertedFunnyTrueToBoolean, convertedFunnyTrueToBoolean);
+
+// 4. Convert funnyFalse to Boolean
+let funnyFalse = '0';
+let convertedFunnyFalseToBoolean = Boolean(funnyFalse);
+console.log(typeof convertedFunnyFalseToBoolean, convertedFunnyFalseToBoolean);
+
+// 5. Convert isTrue to String
+let isTrue = false;
+let convertedIsTrueToString = String(isTrue);
+console.log(typeof convertedIsTrueToString, convertedIsTrueToString);
+
+// 6. Convert isFalse to Number
+let isFalse = true;
+let convertedIsFalseToNumber = Number(isFalse);
+console.log(typeof convertedIsFalseToNumber, convertedIsFalseToNumber);
+
+// 7. Convert funnyText to Number
+let funnyText = 'JavaScript is dynamic programming language.';
+let convertedFunnyTextToNumber = Number(funnyText);
+console.log(typeof convertedFunnyTextToNumber, convertedFunnyTextToNumber);
+
+// 8. Convert funnyNull to String
+let funnyNull = null;
+let convertedFunnyNullToString = String(funnyNull);
+console.log(typeof convertedFunnyNullToString, convertedFunnyNullToString);
+
+// 9. Convert funnyUndefined to String
+let funnyUndefined = undefined;
+let convertedFunnyUndefinedToString = String(funnyUndefined);
+console.log(typeof convertedFunnyUndefinedToString, convertedFunnyUndefinedToString);
+
+// 10. Convert isTrue to String
+let isTrue = true;
+let convertedIsTrueToString = String(isTrue);
+console.log(typeof convertedIsTrueToString, convertedIsTrueToString);
+
+// 11. Convert isFalse to String
+let isFalse = false;
+let convertedIsFalseToString = String(isFalse);
+console.log(typeof convertedIsFalseToString, convertedIsFalseToString);
+
+// 12. Convert booleanString to Number
+let booleanString = 'true';
+let convertedBooleanStringToNumber = Number(booleanString);
+console.log(typeof convertedBooleanStringToNumber, convertedBooleanStringToNumber);
+
+// 13. Convert otherBooleanString to Boolean
+let otherBooleanString = 'false';
+let convertedOtherBooleanStringToBoolean = Boolean(otherBooleanString);
+console.log(typeof convertedOtherBooleanStringToBoolean, convertedOtherBooleanStringToBoolean);
